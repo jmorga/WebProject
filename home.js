dbf778cb038f7c248ac75d31bf9e36ce512d198f
jmorga <32583176+jmorga@users.noreply.github.com>
2020-12-10 13:16:32 -0800

    Add files via upload

---
+++ home.js
@@ -0,0 +1,29 @@
+﻿/// <reference path="knockout-3.5.1.debug.js" />
+
+function AppViewModel() {
+    this.math = ko.observableArray(["+", "-", "*", "/"]);
+    this.firstValue = ko.observable(0);
+    this.secondValue = ko.observable(0);
+    this.symbol = ko.observable();
+
+    this.result = ko.computed(function () {
+        let firstNumber = parseFloat(this.firstValue());
+        let secondNumber = parseFloat(this.secondValue());
+
+        switch (this.symbol()) {
+            case "+": return firstNumber + secondNumber;
+                break;
+            case "-": return firstNumber - secondNumber;
+                break;
+            case "*": return firstNumber * secondNumber;
+                break;
+            case "/": return firstNumber / secondNumber;
+                break;
+            default:
+                return "Somhing went wrong :c";
+        }
+    }, this);
+}
+
+
+ko.applyBindings(new AppViewModel());
